566b9a6be6b11e12041d417f59f038aa921805d9
kevinmsavsani <kevinmsavsani@gmail.com>
2020-07-04 00:16:40 +0530

    book instance view connected to backend

--- src/components/bookinstance-list.component.js
+++ src/components/bookinstance-list.component.js
@@ -1,11 +1,67 @@
-
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const BookInstance = props => (
+  <tr>
+    <td>{props.bookInstance.status}</td>
+    <td>{props.bookInstance.due_back}</td>
+    <td>
+      <Link to={"/edit/"+props.bookInstance._id}>edit</Link> | <a href="#" onClick={() => { props.deleteBookInstance(props.bookInstance._id) }}>delete</a>
+    </td>
+  </tr>
+)
 
 export default class BookInstanceList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteBookInstance = this.deleteBookInstance.bind(this)
+
+    this.state = {bookInstances: []};
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/catalog/bookInstances')
+      .then(response => {
+        this.setState({ bookInstances: response.data })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  deleteBookInstance(id) {
+    axios.delete('http://localhost:5000/bookInstances/'+id)
+      .then(response => { console.log(response.data)});
+
+    this.setState({
+      bookInstances: this.state.bookInstances.filter(el => el._id !== id)
+    })
+  }
+
+  bookInstanceList() {
+    return this.state.bookInstances.map(currentbookInstance => {
+      return <BookInstance bookInstance={currentbookInstance} deleteBookInstance={this.deleteBookInstance} key={currentbookInstance._id}/>;
+    })
+  }
+
   render() {
     return (
       <div>
-        <p>You are on the BookInstance List component!</p>
+        <h3>Logged BookInstances</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Status</th>
+              <th>Due_back</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.bookInstanceList() }
+          </tbody>
+        </table>
       </div>
     )
   }
