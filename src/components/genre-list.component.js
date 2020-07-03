ade86fe473744a4b5bfa56f0243e2b87847016e6
kevinmsavsani <kevinmsavsani@gmail.com>
2020-07-04 00:09:42 +0530

    genre view connected to backend

--- src/components/genre-list.component.js
+++ src/components/genre-list.component.js
@@ -1,11 +1,65 @@
-
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Genre = props => (
+  <tr>
+    <td>{props.genre.name}</td>
+    <td>
+      <Link to={"/edit/"+props.genre._id}>edit</Link> | <a href="#" onClick={() => { props.deleteGenre(props.genre._id) }}>delete</a>
+    </td>
+  </tr>
+)
 
 export default class GenreList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteGenre = this.deleteGenre.bind(this)
+
+    this.state = {genres: []};
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/catalog/genres')
+      .then(response => {
+        this.setState({ genres: response.data })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  deleteGenre(id) {
+    axios.delete('http://localhost:5000/genres/'+id)
+      .then(response => { console.log(response.data)});
+
+    this.setState({
+      genres: this.state.genres.filter(el => el._id !== id)
+    })
+  }
+
+  genreList() {
+    return this.state.genres.map(currentgenre => {
+      return <Genre genre={currentgenre} deleteGenre={this.deleteGenre} key={currentgenre._id}/>;
+    })
+  }
+
   render() {
     return (
       <div>
-        <p>You are on the GenreInstance List component!</p>
+        <h3>Logged Genres</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Name</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.genreList() }
+          </tbody>
+        </table>
       </div>
     )
   }
