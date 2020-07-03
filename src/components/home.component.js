0a2bc110d0c8f58eaf55a28a4d61a723586f1930
kevinmsavsani <kevinmsavsani@gmail.com>
2020-07-04 00:05:19 +0530

    home view connected to backend

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,17 +1,34 @@
 
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Home extends Component {
+     constructor(props) {
+        super(props);
+
+        this.state = {data: []};
+      }
+
+      componentDidMount() {
+        axios.get('http://localhost:5000/catalog/')
+          .then(response => {
+            this.setState({ data: response.data })
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+      }
+
   render() {
     return (
       <div class="col-sm-10">
         <p>The library has the following record counts:</p>
         <ul>
-          <li><strong>Books:</strong> "data.book_count"</li>
-          <li><strong>Copies:</strong> "data.book_instance_count"</li>
-          <li><strong>Copies available:</strong> "data.book_instance_available_count"</li>
-          <li><strong>Authors:</strong> "data.author_count"</li>
-          <li><strong>Genres:</strong> "data.genre_count"</li>
+          <li><strong>Books:</strong> {this.state.data.book_count} </li>
+          <li><strong>Copies:</strong> {this.state.data.book_instance_count} </li>
+          <li><strong>Copies available:</strong> {this.state.data.book_instance_available_count} </li>
+          <li><strong>Authors:</strong> {this.state.data.author_count} </li>
+          <li><strong>Genres:</strong> {this.state.data.genre_count} </li>
         </ul>
       </div>
     )
